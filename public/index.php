cbbc71c253dddd84e67a8d6f2b5cf7b5485e5b85
ShiroNatsu22 <aushojgs@gmail.com>
2022-03-06 11:31:37 +0100

    use routes from different bundles

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-use Skeleton\Apps\Skeleton\SkeletonKernel;
+use Skeleton\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new SkeletonKernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
